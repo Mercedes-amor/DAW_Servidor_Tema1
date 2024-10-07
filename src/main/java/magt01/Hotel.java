4586445fc388c13c562151d7f54f6c231e9d01c9
MercedesAmorGallart <127596167+MercedesAmorGallart@users.noreply.github.com>
2024-10-07 11:08:35 +0200

    Tema 01, ejercicisos 1.4 opcional hecho

---
+++ src/main/java/magt01/Hotel.java
@@ -0,0 +1,32 @@
+/*
+ * TEMA 1: Ejercicio 1.4 (Opcional)
+ */
+package magt01;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mercedes Amor
+ */
+public class Hotel {
+    private ArrayList<Habitacion> listaHabitaciones = new ArrayList<>();
+
+    public Hotel() {
+    }
+    
+
+    //Getters y Setters
+    public ArrayList<Habitacion> getListaHabitaciones() {
+        return listaHabitaciones;
+    }
+
+    public void setListaHabitaciones(ArrayList<Habitacion> listaHabitaciones) {
+        this.listaHabitaciones = listaHabitaciones;
+    }
+    
+    
+    
+    
+    
+}
